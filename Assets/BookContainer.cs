b3d805fbf37285a93e7f5d987118065134242773
LawrenceBarone <74180953+LawrenceBarone@users.noreply.github.com>
2023-06-28 13:00:11 +0200

    Book container

--- Assets/BookContainer.cs
+++ Assets/BookContainer.cs
@@ -7,9 +7,29 @@ public class BookContainer : MonoBehaviour
 {
     TextMeshProUGUI numberOfBooksTxt;
     [SerializeField] int _nbOfBooks = 1;
-    
+    [SerializeField] int _totalNbOfBooks = 1;
+
+    bool regenerate = false;
     public void UpdateNbOfBooks()
     {
+        if(_nbOfBooks > 0) _nbOfBooks--;
+    }
 
+    private void FixedUpdate()
+    {
+        if (_totalNbOfBooks > _nbOfBooks)
+        {
+            regenerate = true;
+            RegenerateBook();
+        }
+        else regenerate = false;
+    }
+
+    void RegenerateBook()
+    {
+        if (regenerate)
+        {
+            _nbOfBooks++;
+        }
     }
 }
