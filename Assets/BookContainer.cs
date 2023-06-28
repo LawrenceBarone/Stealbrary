d9f3d671042a7b295650464b103f167fc2ba10db
LawrenceBarone <74180953+LawrenceBarone@users.noreply.github.com>
2023-06-28 12:39:53 +0200

    Fancy game shit, its cool

---
+++ Assets/BookContainer.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BookContainer : MonoBehaviour
+{
+    TextMeshProUGUI numberOfBooksTxt;
+    [SerializeField] int _nbOfBooks = 1;
+    
+    public void UpdateNbOfBooks()
+    {
+
+    }
+}
